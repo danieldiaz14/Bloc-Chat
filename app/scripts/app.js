3c7064585e327f4d2d4553d7eab877ef73791864
Danny Diaz <danieldial129@gmail.com>
2017-05-23 19:08:54 -0700

    Create Chat rooms updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,20 @@
 (function() {
-    function config($locationProvider, $stateProvider) {
+    function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: true,
                 requireBase: false
-    });
+            });
+        
         $stateProvider
             .state('home', {
                 url: '/',
                 controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
-        });
-        
+            });
     }
     
     angular
-        
-        .module('blocChat', ['ui.router', 'firebase'])
+        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
         .config(config);
 })();
